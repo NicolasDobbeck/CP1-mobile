f25bbf74eb857bbf4e572e2891c651d135389ead
NicolasDobbeck <nicolasdobbeckmendes12@gmail.com>
2025-03-14 20:35:10 -0300

    Redimencionando os components

--- App.js
+++ App.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#1484ED',
     alignItems: 'center',
-    justifyContent: 'start',
+    justifyContent: 'flex-start',
   },
   imagem: {
     resizeMode: 'center',
